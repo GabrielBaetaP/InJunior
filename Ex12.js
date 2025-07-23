794c078b1265449a54facd78795c5b76339a2642
Gabriel Baeta <148347288+GabrielBaetaP@users.noreply.github.com>
2025-07-22 21:24:32 -0300

    Delete Ex12.js

--- Ex12.js
+++
@@ -1,7 +0,0 @@
-function encontrarElementoUnico(lista){
-    return lista.reduce((numeros, numeroAtual) => numeros ^ numeroAtual, 0);
-}
-
-const vet = [1,2,1,5,6,6,8,2,3,8,5];
-
-console.log(encontrarElementoUnico(vet));
