bc6fa1b49f08809cf1acbc09d4b4f7a1f8b45319
Gabriel Baeta <gabrielbaeta@id.uff.br>
2025-08-01 16:49:34 -0300

    fix: mudança de lógica e adição do rating

--- Tarefa API e Promises/web/PagPrincipal.js
+++ Tarefa API e Promises/web/PagPrincipal.js
@@ -14,35 +14,27 @@ fetch(apiUrl)
             const cardDoProduto = document.createElement('div');
             cardDoProduto.className = 'card-produto';
 
-            const imagem = document.createElement('img');
-            imagem.src = 'https://acdn-us.mitiendanube.com/stores/001/409/577/products/camisa-reserva-flamengo-adidas-branco-2022-2023-kit-2-crf-torcedor-masculino-11-8fc7ec252f417b940416553866558140-640-0.jpg';
-
-            const informacoes = document.createElement('div');
-            informacoes.className = 'informacoes';
-
-            const nome = document.createElement('p');
-            nome.className = 'titulo';
-            nome.textContent = produto.name;
-
-            const categoria = document.createElement('p');
-            categoria.className = 'categoria';
-            categoria.textContent = produto.category;
-
-            const descricao = document.createElement('p');
-            descricao.className = 'descricao';
-            descricao.textContent = produto.description; 
-
-            const preco = document.createElement('p');
-            preco.className = 'preco';
-            preco.textContent = `R$ ${produto.price.toFixed(2).replace('.', ',')}`;
-
-            informacoes.appendChild(nome);
-            informacoes.appendChild(categoria);
-            informacoes.appendChild(descricao);
-            informacoes.appendChild(preco);
+            cardDoProduto.innerHTML =`
+            <div class="card">
+                <div class="parte-imagem">
+                    <div class="cima">
+                        <p class="rating">${produto.rating.toFixed(1)} <img src="./assets/estrela.png"></p>
+                        <div class="botoes">
+                            <button id="btn-remover"><img src="./assets/remover.png"></button>
+                            <button id="btn-editar"><img src="./assets/editar.png"></button>
+                        </div>
+                    </div>
+                    <img src="https://acdn-us.mitiendanube.com/stores/001/409/577/products/camisa-reserva-flamengo-adidas-branco-2022-2023-kit-2-crf-torcedor-masculino-11-8fc7ec252f417b940416553866558140-640-0.jpg"">
+                </div>
             
-            cardDoProduto.appendChild(imagem);
-            cardDoProduto.appendChild(informacoes);
+                <div class="informacoes">
+                    <p class="titulo">${produto.name}</p>
+                    <p class="categoria">${produto.category}</p>
+                    <p class="descricao">${produto.description}</p>
+                    <p class="preco">R$ ${produto.price.toFixed(2).replace('.', ',')}</p>
+                </div>
+            </div>
+            `;
 
             containerDeProdutos.appendChild(cardDoProduto);
         });
